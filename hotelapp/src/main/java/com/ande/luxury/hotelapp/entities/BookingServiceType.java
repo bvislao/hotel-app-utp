c90a3a7149298eb76298c2ca7bea9f94c6bf1219
jfernandezyf <u22248260@utp.edu.pe>
2025-06-14 17:38:12 -0500

    entites booking service type

---
+++ hotelapp/src/main/java/com/ande/luxury/hotelapp/entities/BookingServiceType.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ande.luxury.hotelapp.entities;
+
+import lombok.Data;
+
+/**
+ *
+ * @author ferfer
+ */
+@Data
+public class BookingServiceType extends Auditable {
+    private Integer id;
+    private String uuid;
+    
+}
